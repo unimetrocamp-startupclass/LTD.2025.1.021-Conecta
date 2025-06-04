cf0e8984b90fd8fae201e2c81fd9de2f938f18d6
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2025-06-04 19:54:36 -0300

    Arquivo atualizado.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Description: Classe Usuario para gerenciar usuários com armazenamento local
+// e autenticação simples.
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -7,12 +9,14 @@ const multer = require('multer');
 const path = require('path');
 
 const authRoutes = require('./routes/auth');
-const projetoRoutes = require('./routes/projetos');
+const projetoRoutes = require('./routes/projetosback');
 
+// Configuração do dotenv para carregar variáveis de ambiente
 dotenv.config();
 const app = express();
 const PORT = 3000;
 
+// Configuração do multer para upload de arquivos
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB conectado'))
   .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
@@ -22,7 +26,11 @@ app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', authRoutes);
 app.use('/api/projetos', projetoRoutes);
+app.get('/', (req, res) => {
+  res.send('Servidor está funcionando! ✅');
+});
 
+// Middleware para lidar com erros
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
